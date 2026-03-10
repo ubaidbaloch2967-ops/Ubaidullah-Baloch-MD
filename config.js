f434e793eb3d8dee5f6832b3f23bc23b8f3fa245
Ubaidullah_BaLoch <ubaidbaloch2967@gmail.com>
2026-03-10 13:53:34 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    botName: "Ubaidullah-Baloch-MD",
+    ownerName: "𖣔፝͜͡☞ 𝙐𝙗𝙖𝙞𝙙𝙪𝙡𝙡𝙖𝙝 𝘽𝙖𝙡𝙤𝙘𝙝",
+    ownerNumber: "923702866763",
+    prefix: ".",
+    thumb: "https://image2url.com/r2/default/images/1773132494378-437ae601-d8a1-4a78-b100-247726646b3d.png",
+    footer: "© Powered By Ubaidullah 03702866763"
+};
